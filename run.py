072be3d0a0b87deb36fb5f1aab34dfcfa9fec4b8
Yahya Abou Imran <yahya-abou-imran@protonmail.com>
2020-02-25 10:36:20 +0100

    Make it work together

--- run.py
+++ run.py
@@ -1,37 +1,35 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Flask, render_template, url_for
+from models import db
 
-app = Flask(__name__, template_folder="modules/templates")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/djamel/PycharmProjects/traduction/test.db'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
-def create_app():
-    app = Flask(__name__)
-    db.init_app(app)
-    return app
+app = Flask(
+    __name__,
+    template_folder="app/templates/",
+    static_folder="app/templates/"
+)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db.init_app(app)
 
-@app.before_first_request
-def create_tables():
+
+with app.app_context():
+    db.drop_all()
     db.create_all()
-
-app = create_app()
-app.app_context().push()
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80),    nullable=False)
-    email = db.Column(db.String(120),    nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
 
 
 @app.route('/')
 def index():
+    return render_template('index.html')
 
-    return render_template('./templates/index.html')
+
+@app.route('/traductions')
+def traductions():
+    return render_template('traductions.html')
+
+
+@app.route('/score')
+def score():
+    return render_template('score.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=8080)
