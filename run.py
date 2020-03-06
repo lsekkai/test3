a59be51e4393cdd500851ce9de63d722f2e65814
djamel_wilab <epstodjamel>
2020-03-06 08:30:28 +0100

    todolist is completed done !!

--- run.py
+++ run.py
@@ -30,6 +30,10 @@ if not database_exists(DB_URI):
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 @app.route('/traduction', methods=['GET', 'POST'])
 def traduction():
